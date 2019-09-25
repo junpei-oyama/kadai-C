ff32066e5adb3c8644a09831d409e691ba829b30
junpei-oyama <junpei.oyama.ai@gmail.com>
2019-09-25 15:20:23 +0900

    【課題C-2】NGワードを<censored>に変換するメソッドをWordFilterクラスに追加した
    - replaceメソッドを利用して文字列の置換を行った。

--- wordfilter.py
+++ wordfilter.py
@@ -5,9 +5,13 @@ class WordFilter:
     def detect(self, in_word):
         return self.ng_word in in_word
 
+    def censor(self, in_word):
+        cens_str = in_word.replace(self.ng_word, '<censored>')
+        return cens_str
+
 
 def main():
-    in_str = '昨日のアーセナルの試合アツかった！'
+    in_str = "昨日のアーセナルの試合アツかった！"
 
     my_filter = WordFilter("アーセナル")
 
@@ -16,6 +20,12 @@ def main():
 
     # NGワードが含まれていない場合
     my_filter.detect("昨日のリバプールの試合アツかった！")  # Falseを返す ※出力されるわけではありません！
+
+    # NGワードが含まれている場合
+    print(my_filter.censor("昨日のアーセナルの試合アツかった！"))  # "昨日の<censored>の試合アツかった！" を返す ※出力されるわけではありません！
+
+    # NGワードが含まれていない場合
+    print(my_filter.censor("昨日のリバプールの試合アツかった！"))  # "昨日のリバプールの試合アツかった！" を返す ※出力されるわけではありません！
 
 
 if __name__ == '__main__':
