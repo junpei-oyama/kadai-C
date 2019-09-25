b35c86630c1593194e2d219ba0fac452d822bdd4
junpei-oyama <junpei.oyama.ai@gmail.com>
2019-09-25 16:10:01 +0900

    【課題C-4】複数のNGワードを登録できるようにした。
    - インスタンスを作成するときの引数としてNGワードのリストを渡すようにした。
    - censorメソッドをリストで置換できるようにするところで時間が掛かった。
    - detectメソッドは複数のBGワードの内1つでも含まれていればTrueを返すように変更した。

--- wordfilter.py
+++ wordfilter.py
@@ -3,19 +3,27 @@ class WordFilter:
         self.ng_word = ng_word
 
     def detect(self, in_word):
-        return self.ng_word in in_word
+        for i in self.ng_word:
+            if i in in_word:
+                return True
+
+        return False
 
     def censor(self, in_word, rep):
-        cens_str = in_word.replace(self.ng_word, rep)
-        return cens_str
+        for i in self.ng_word:
+            print(i)
+            in_word = in_word.replace(i, rep)
+
+        return in_word
 
 
 def main():
     # 入力
     in_str = "昨日のアーセナルの試合アツかった！"
     rep_word = "<見せられないよ!>"
+    ng_list = ['アーセナル', 'アツかった']
 
-    my_filter = WordFilter("アーセナル")
+    my_filter = WordFilter(ng_list)
 
     # NGワードが含まれている場合
     my_filter.detect(in_str)  # Trueを返す ※出力されるわけではありません！
